554d2e2c646dcbb732431bd992dde37f425099ef
kheperV3 <louis@ros.fr>
2018-06-12 13:55:53 +0200

    Update action-louisros.Timer.py

--- action-louisros.Timer.py
+++ action-louisros.Timer.py
@@ -45,6 +45,7 @@ def stoptimer_callback(hermes, intentMessage):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("louisros:settimer",settimer_callback).start()
-        h.subscribe_intent("louisros:stoptimer",stoptimer_callback).start()
+        h\
+        .subscribe_intent("louisros:settimer",settimer_callback)\
+        .subscribe_intent("louisros:stoptimer",stoptimer_callback).start()
        
