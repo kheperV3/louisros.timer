6312e2ab54ced90c45f02f1e8e5214d149f5afeb
kheperV3 <louis@ros.fr>
2018-06-13 19:30:44 +0200

    Update action-louisros.Timer.py

--- action-louisros.Timer.py
+++ action-louisros.Timer.py
@@ -32,10 +32,10 @@ def settimer_callback(hermes, intentMessage):
   
     v = int(intentMessage.slots.valeur.first().value) * 60 
     
-    os.system("echo " + str(v) + " >/var/lib/snips/skills/timeForAlarm")  
-    os.system("echo 25 >/sys/class/gpio/export")
-    os.system("echo out >/sys/class/gpio/gpio25/direction")
-    os.system("echo 1 >/sys/class/gpio/gpio25/value")
+    os.system("sudo echo " + str(v) + " >/var/lib/snips/skills/timeForAlarm")  
+    os.system("sudo echo 25 >/sys/class/gpio/export")
+    os.system("sudo echo out >/sys/class/gpio/gpio25/direction")
+    os.system("sudo echo 1 >/sys/class/gpio/gpio25/value")
     
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, "c'est fait cher Maître")
